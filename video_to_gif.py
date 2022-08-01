8d9c3d861996631ae7a1b2a9af90eff543e9b23f
ILIAS <nossudak@gmail.com>
2022-08-01 14:29:10 +0300

    Error handling was added

--- video_to_gif.py
+++ video_to_gif.py
@@ -3,11 +3,16 @@ from moviepy.editor import VideoFileClip
 
 
 VIDEO_FILE_NAME = "video.mp4"
-url_link = input("Enter video url:")
+url_link = input("Enter video url: ")
 try:
     urllib.request.urlretrieve(url_link, VIDEO_FILE_NAME)
 except ValueError:
     print("Impossible to download this video")
     exit()
-videoClip = VideoFileClip(VIDEO_FILE_NAME)
-videoClip.write_gif("test.gif")
+try:
+    videoClip = VideoFileClip(VIDEO_FILE_NAME)
+    videoClip.write_gif("test.gif")
+except:
+    print("Invalid video. GIF was not created")
+else:
+    print("GIF created successfully")
