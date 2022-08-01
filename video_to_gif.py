c7efc9809e43a4be2764a43d727d43390d51d7ca
ILIAS <nossudak@gmail.com>
2022-08-01 14:20:12 +0300

    Video to gif task is done.

---
+++ video_to_gif.py
@@ -0,0 +1,13 @@
+import urllib.request
+from moviepy.editor import VideoFileClip
+
+
+VIDEO_FILE_NAME = "video.mp4"
+url_link = input("Enter video url:")
+try:
+    urllib.request.urlretrieve(url_link, VIDEO_FILE_NAME)
+except ValueError:
+    print("Impossible to download this video")
+    exit()
+videoClip = VideoFileClip(VIDEO_FILE_NAME)
+videoClip.write_gif("test.gif")
